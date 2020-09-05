07ebab7dfe8903ed8a48f75b07b7e4e3dc9b588e
Juanes <jedp082@gmail.com>
2020-09-04 21:24:45 -0500

    feature/option - created the component Option  with CurrentOptions and visual tests

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,9 +5,11 @@ import { WrapperHome } from './styles'
 
 import { Rules } from '../../components/rules'
 import { Header } from '../../components/header'
+import { Option } from '../../components/option'
 
 export const Home = () => (
   <WrapperHome>
+    <Option option="rock" color="blue" />
     <Header />
     <Rules title="Rules" img={ImageRules} />
   </WrapperHome>
